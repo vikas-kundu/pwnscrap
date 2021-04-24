cd3dc6f7690b2c821c2848029e10bdf74f098c17
vikas-kundu <47297799+vikas-kundu@users.noreply.github.com>
2021-04-24 21:52:26 +0530

    Minor improvements

--- pwnscrap.py
+++ pwnscrap.py
@@ -3,8 +3,6 @@ import json
 import argparse
 import cloudscraper
 
-regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
-email = ""
 
 def parse():
 	parser = argparse.ArgumentParser(description='Check if your email has been pwned')
@@ -14,23 +12,24 @@ def parse():
 
 
 def scrap_and_store(email):
-	url = "https://haveibeenpwned.com/unifiedsearch/"+email
+	url = f"https://haveibeenpwned.com/unifiedsearch/{email}"
 	scraper = cloudscraper.create_scraper()
-	result = scraper.get(url)
-	print("Info: Done Scrapping!")
-	if(result.text!=""):
-		print("Info: Breach found!")
-		with open("data/{}.json".format(email), "w") as f:
-			json.dump(result.json(), f , sort_keys=True, indent=4)
-		print("Info: Data saved at {}.json".format(email))
+	try:
+		result = scraper.get(url)
+	except Exception as e:
+		print(f"Error: Following exception occured!\n{e}\nPlease retry once more!")
+
 	else:
-		print("Info: No breach found for this email.")
+		print('Info: Done Scrapping!')
+		if result.text!='':
+			print('Warning: Breach found!')
+			with open("data/{}.json".format(email), "w") as f:
+				json.dump(result.json(), f , sort_keys=True, indent=4)
+			print(f"Info: Data saved at /data/{email}.json")
+		else:
+			print('Info: No breach found for this email.')
     
 if __name__ == "__main__":
 	email = parse().email
-	if(re.search(regex,email)):  
-		scrap_and_store(email)
-	else:
-		print("Error: Invalid Email please try again with valid mail!")
-
+	scrap_and_store(email)
 
